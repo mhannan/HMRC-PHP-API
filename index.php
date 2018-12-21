86596c88af62c167d1bbefadb011e380b1f57e93
mhannan <m.hannan313@gmail.com>
2018-12-21 12:25:40 +0500

    create index.php
    Main entry point.

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require('./functions.php');
+
+HMRC_helloWorld();    // Testing
+
+
+/* 1. Create User
+* --------------------------------------
+* point5: https://developer.service.hmrc.gov.uk/api-documentation/docs/testing
+*/
+
+HMRC_createUser();
